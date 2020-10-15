f2baa9e67662c38e61c40ed6b06f5d974e18b5c8
Neha Sanserwal <neha16sanserwal@gmail.com>
2020-10-15 09:45:02 +0530

    Overrided equals method

--- day2/Geometry.java
+++ day2/Geometry.java
@@ -16,14 +16,41 @@ class Point {
   public String toString() {
     return String.format("Point@ { x: " + this.x + ", y: " + this.y + " }");
   }
+
+  @Override
+  public boolean equals(Object other) {
+    if (!(other instanceof Point)) {
+      return false;
+    }
+    if (other == this) {
+      return true;
+    }
+    Point otherPoint = (Point) other;
+    if (this.x == otherPoint.x && this.y == otherPoint.y) {
+      return true;
+    }
+    return false;
+  }
 }
 
 public class Geometry {
 
   public static void main(String[] args) {
-    Point pointAtZero = new Point();
+    Point zeroPoint = new Point();
     Point nonZeroPoint = new Point(2, 4);
-    System.out.println("Point at zero :" + pointAtZero.toString());
+    Point otherZeroPoint = new Point();
+    System.out.println("Point at zero :" + zeroPoint.toString());
     System.out.println("Non-zero point :" + nonZeroPoint);
+    System.out.println(
+      "zeroPoint and otherZeroPoint are equal? :" +
+      zeroPoint.equals(otherZeroPoint)
+    );
+    System.out.println(
+      "zeroPoint and nonZeroPoint are equal? :" + zeroPoint.equals(nonZeroPoint)
+    );
+
+    System.out.println(
+      "nonZeroPoint and null are equal? :" + nonZeroPoint.equals(null)
+    );
   }
 }
