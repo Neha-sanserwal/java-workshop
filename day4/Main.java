58c1516148a033b85f21d7813dfa7839e0a4b10a
Neha Sanserwal <neha16sanserwal@gmail.com>
2020-10-20 08:56:10 +0530

    created package lib/math

--- day4/Main.java
+++ day4/Main.java
@@ -1,4 +1,9 @@
+import math.geometry.Point;
+
 public class Main {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Point p1 = new Point();
+    System.out.println(p1);
+  }
 }
---
+++ lib/mathlib/geometry/Point.java
@@ -0,0 +1,8 @@
+package mathlib.geometry;
+
+public class Point {
+
+  Point() {
+    System.out.println("this is Point");
+  }
+}
