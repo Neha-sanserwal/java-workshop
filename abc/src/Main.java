0e029336e430b2fd90a1e5b4b47e053a938bfb03
Neha Sanserwal <neha16sanserwal@gmail.com>
2020-10-27 20:50:31 +0530

    added gitignore

---
+++ abc/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] ints = {1, 3, 3};
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println("ints = " + ints[i]);
+        }
+    }
+}
